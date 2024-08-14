a8a96b1ac2740d8079fa2a63b5493024872056fc
DESKTOP-D1KGUPP\DenisHsu <denishsu612@gmail.com>
2024-08-14 23:46:55 +0800

    Revert "製作API"
    This reverts commit e5ea5c949fd16f7ca1d238467dfb71ea24ee57d2.

--- meeting_test/meeting_test/Program.cs
+++ meeting_test/meeting_test/Program.cs
@@ -1,12 +1,9 @@
-using Microsoft.EntityFrameworkCore;
-using meeting_test.Models;
-
-
 var builder = WebApplication.CreateBuilder(args);
 
+// Add services to the container.
+
 builder.Services.AddControllers();
-builder.Services.AddDbContext<RevenueContext>(options =>
-options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnectionString")));
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
